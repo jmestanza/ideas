3377a0b6a5d11b6ad404a0f1c4f2917fd4a79495
jmestanza <jmestanza@itba.edu.ar>
2018-11-02 15:09:06 -0300

    added algebrasdfgsae

---
+++ Álgebra/algebra2.m
@@ -0,0 +1,24 @@
+clear;
+clc;
+x = transpose([1;2]);
+y = [3;-1];
+
+A = [[3;1],[1;2]];
+
+answer = x*A*y;
+
+answer2 = x*A*transpose(x);
+
+syms v1;
+syms v2;
+proy = (10/15)*(v1+2*v2);
+
+u= 3*v1 - v2;
+
+u-proy
+
+x = transpose([7/3 ; -7/3]);
+
+
+answer3 = x*A*transpose(x) 
+
