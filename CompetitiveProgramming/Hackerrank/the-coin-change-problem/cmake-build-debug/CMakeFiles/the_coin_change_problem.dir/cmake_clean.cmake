8a6c3d8b4f447b3d430c36e06ca624c49fb70571
jmestanza <jmestanza@itba.edu.ar>
2021-10-02 17:09:53 -0300

    should optimize coin change problem

---
+++ CompetitiveProgramming/Hackerrank/the-coin-change-problem/cmake-build-debug/CMakeFiles/the_coin_change_problem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/the_coin_change_problem.dir/main.cpp.obj"
+  "libthe_coin_change_problem.dll.a"
+  "the_coin_change_problem.exe"
+  "the_coin_change_problem.exe.manifest"
+  "the_coin_change_problem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/the_coin_change_problem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
